8af2b8a1788714d30bd12980c8150240c453a32c
jia-bing-xin <441026068@qq.com>
2022-09-11 16:55:10 +0800

    封装map

--- src/views/Map/index.vue
+++ src/views/Map/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>地图</div>
+  <div class="map" ref="map">
+    <amap />
+  </div>
 </template>
 <script setup lang="ts">
-
+import amap from '@/component/amap/index.vue'
+import { ref, onMounted } from 'vue'
+const map = ref(null) // 通过 ref 绑定子组件
+const getSonComponent = () => {
+  console.log(map.value)
+}
+onMounted(() => {
+  getSonComponent()
+})
 </script>
 <style lang="scss" scoped>
+.map {
+  width: 100%;
+  height: calc(100vh - 90px);
+}
 </style>
