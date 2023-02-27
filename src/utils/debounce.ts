ef00fd397186324d364ac70f82f5c1640b3350f2
jia-bing-xin <441026068@qq.com>
2023-02-27 19:57:08 +0800

    封装防抖与节流

---
+++ src/utils/debounce.ts
@@ -0,0 +1,23 @@
+/**
+ * @description: 防抖
+ * @param {*} fn   回调
+ * @param {*} delay  延迟时间
+ * @param {*} immediate 是否立即执行
+ */
+export function debounce(fn, delay = 500, immediate = false) {
+    let timer = null;
+    return (...arg) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        //是否立即执行
+        if (immediate && !timer) {
+            fn(...arg)
+            timer = true
+        } else {
+            timer = setTimeout(() => {
+                fn(...arg)
+            }, delay);
+        }
+    }
+}
---
+++ src/utils/throttled.ts
@@ -0,0 +1,20 @@
+/**
+ * @description: 
+ * @param {*} fn 回调
+ * @param {*} delay 等待时间
+ */
+export function throttled(fn, delay) {
+    let timer = null
+    let starttime = Date.now()
+    return (...arg) => {
+        let curTime = Date.now() // 当前时间
+        let remaining = delay - (curTime - starttime)  // 从上一次到现在，还剩下多少多余时间
+        clearTimeout(timer)
+        if (remaining <= 0) {
+            fn(...arg)
+            starttime = Date.now()
+        } else {
+            timer = setTimeout(fn, remaining);
+        }
+    }
+}
