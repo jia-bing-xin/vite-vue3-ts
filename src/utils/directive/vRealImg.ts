d3636de51253ab1d191653d4c875b40e82124a4c
jia-bing-xin <441026068@qq.com>
2023-03-01 15:52:49 +0800

    图片请求失败的时候显示另一张图片

---
+++ src/utils/directive/vRealImg.ts
@@ -0,0 +1,29 @@
+/**
+ * @description: 图片请求失败的时候显示另一张图片:<img v-real-img="'images/logo.png'" src="images/errorLogo.png"/>
+ */
+export const vRealImg = {
+    beforeMount: async (el, binding) => {
+        const imgURL = binding.value;
+        if (imgURL) {
+            const exist = await imageIsExist(imgURL);
+            exist && el.setAttribute('src', imgURL);
+        }
+    }
+}
+// 判断一个图片是否存在, 注意是异步行为
+function imageIsExist(url) {
+    return new Promise(resolve => {
+        let img = new Image();
+        img.src = url;
+        img.onload = () => {
+            if (img.complete) {
+                resolve(true);
+                img = null;
+            }
+        }
+        img.onerror = () => {
+            resolve(false);
+            img = null;
+        }
+    })
+}
