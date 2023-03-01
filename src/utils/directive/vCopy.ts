343da1a728d2ad47237010eabd777e44c0a13706
jia-bing-xin <441026068@qq.com>
2023-03-01 15:52:25 +0800

    拷贝指令

--- src/utils/directive/vCopy.ts
+++ src/utils/directive/vCopy.ts
@@ -1,33 +1,33 @@
+/**
+ * @description: copy
+ * @param {String | Number}: copy的值
+ */
+import { ElMessage } from 'element-plus'
 export const vCopy = {
-    beforeMount(el, binding) {
+    mounted: (el, binding) => {
         el.targetContent = binding.value;
-        const success = binding.arg;
         el.addEventListener('click', () => {
-            if (!el.targetContent) return console.warn('没有需要复制的目标内容');
-            // 创建textarea标签
-            const textarea = document.createElement('textarea');
-            // 设置相关属性
-            // textarea.readOnly = 'readonly';
-            textarea.style.position = 'fixed';
-            textarea.style.top = '-99999px';
-            // 把目标内容赋值给它的value属性
-            textarea.value = el.targetContent;
-            // 插入到页面
-            document.body.appendChild(textarea);
-            // 调用onselect()方法
-            textarea.select();
-            // 把目标内容复制进剪贴板, 该API会返回一个Boolean
-            const res = document.execCommand('Copy');
-            res && success ? success(el.targetContent) : console.log('复制成功，剪贴板内容：' + el.targetContent);
-            // 移除textarea标签
-            document.body.removeChild(textarea);
+            //document.execCommand('Copy');废弃
+            navigator.clipboard.writeText(el.targetContent).then(() => {
+                ElMessage({
+                    showClose: true,
+                    message: '复制成功。',
+                    type: 'success',
+                })
+            }).catch(() => {
+                ElMessage({
+                    showClose: true,
+                    message: '复制失败。',
+                    type: 'error',
+                })
+            })
         })
     },
-    updated(el, binding) {
+    updated: (el, binding) => {
         // 实时更新最新的目标内容
         el.targetContent = binding.value;
     },
-    unmounted(el) {
-        el.removeEventListener('click', () => { })
+    unmounted: (el) => {
+        el.removeEventListener('click')
     }
 }
